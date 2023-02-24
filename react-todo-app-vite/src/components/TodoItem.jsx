72232a50e71759a83a01ec85b4d731507aa235ad
madyanes <madyanes.1995@gmail.com>
2023-02-24 16:19:58 +0700

    passing todo prop from TodosList to TodoItem

---
+++ react-todo-app-vite/src/components/TodoItem.jsx
@@ -0,0 +1,7 @@
+const TodoItem = ({ itemProp }) => {
+  return (
+    <li>{ itemProp.title }</li>
+  )
+}
+
+export default TodoItem
--- react-todo-app-vite/src/components/TodosList.jsx
+++ react-todo-app-vite/src/components/TodosList.jsx
@@ -1,8 +1,10 @@
+import TodoItem from '@/components/TodoItem'
+
 const TodosList = ({ todosProps }) => {
   return (
     <ul>
       { todosProps.map((todo) => (
-        <li>{ todo.title }</li>
+        <TodoItem itemProp={ todo } />
       )) }
     </ul>
   )
