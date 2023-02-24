d7fc22c41baa6424f23bbea802bcf80d4ef57b9f
madyanes <madyanes.1995@gmail.com>
2023-02-24 07:52:24 +0700

    using JSX to build a virtual DOM

--- app.js
+++ app.js
@@ -19,7 +19,19 @@ const update = () => {
       new Date().toLocaleTimeString()
     )
   );
+
+  // the alternative way to create a react component is using JSX, but you can't directly execute JSX on a browser.
+  const element2 = (
+    <div>
+      <form>
+        <input type="text" />
+      </form>
+      <span>Time: {new Date().toLocaleTimeString()}</span>
+    </div>
+  );
+
   root.render(element1);
+  root.render(element2);
 };
 
 setInterval(update, 1000);
