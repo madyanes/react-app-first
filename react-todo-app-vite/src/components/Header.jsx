a929cb0619fe1a755030e5b52d0c70690ea72d8e
madyanes <madyanes.1995@gmail.com>
2023-02-25 14:13:55 +0700

    using inline css

--- react-todo-app-vite/src/components/Header.jsx
+++ react-todo-app-vite/src/components/Header.jsx
@@ -1,7 +1,15 @@
 const Header = () => {
   return (
     <header>
-      <h1>Todos</h1>
+      <h1 
+        style={{
+          padding: '20px 0',
+          lineHeight: '1.5em',
+          color: '#aeadad',
+          textAlign: 'center'
+        }}>
+        Todos
+      </h1>
       <p>Items will persist in the browser local storage.</p>
     </header>
   )
