8cb941d7c3e6831ada01f27bd50acd00830083e5
madyanes <madyanes.1995@gmail.com>
2023-02-25 14:27:14 +0700

    applying css through "style", a css object instance

--- react-todo-app-vite/src/components/Header.jsx
+++ react-todo-app-vite/src/components/Header.jsx
@@ -1,13 +1,18 @@
+import styles from '@/styles/Header.module.css'
+
 const Header = () => {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  }
+  
   return (
     <header>
       <h1 
-        style={{
-          padding: '20px 0',
-          lineHeight: '1.5em',
-          color: '#aeadad',
-          textAlign: 'center'
-        }}>
+        style={ headerStyle }
+        className={ styles.header }>
         Todos
       </h1>
       <p>Items will persist in the browser local storage.</p>
