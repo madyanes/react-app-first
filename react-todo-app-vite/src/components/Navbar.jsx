0c3c87ee448c91f64b32519ce059a0ac970ccff7
madyanes <madyanes.1995@gmail.com>
2023-02-27 08:08:59 +0700

    add navigation links

--- react-todo-app-vite/src/components/Navbar.jsx
+++ react-todo-app-vite/src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+  { path: 'profile', text: 'Profile' },
+  { path: 'login', text: 'Login' },
+]
+
 const Navbar = () => {
-  return <div>Navigation items here</div>
+  return (
+    <nav className="navbar">
+      <ul>
+        { links.map((link) => {
+          return (
+            <li key={ link.text }>
+              <a href={ link.path }>{ link.text }</a>
+            </li>
+          )
+        }) }
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
