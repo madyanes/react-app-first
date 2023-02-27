734eca85dc6d070e73bb1c885cd466413358c6e1
madyanes <madyanes.1995@gmail.com>
2023-02-27 09:55:38 +0700

    add login form

--- react-todo-app-vite/src/routes/Login.jsx
+++ react-todo-app-vite/src/routes/Login.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+import styles from '@/styles/Login.module.css'
+
 const Login = () => {
-  return <div>Login page</div>;
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (!username) return;
+    console.log(username)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <div className={ styles.formWrapper }>
+        <form className={ styles.form } onSubmit={ handleSubmit }>
+          <input 
+            type="text" 
+            placeholder="username" 
+            value={ username } 
+            onChange={ (e) => setUsername(e.target.value) } />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
