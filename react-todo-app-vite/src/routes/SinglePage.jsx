b4727845b60ceb632f1cc3bdc6373b43b5afed0c
madyanes <madyanes.1995@gmail.com>
2023-02-27 09:28:21 +0700

    reading url params with useParams() hook

--- react-todo-app-vite/src/routes/SinglePage.jsx
+++ react-todo-app-vite/src/routes/SinglePage.jsx
@@ -1,7 +1,27 @@
+import { useParams } from 'react-router-dom'
+
+const aboutData = [
+  {
+    slug: 'about-app',
+    title: 'About the app',
+    description: "This application lets us add to-dos, edit, and delete items. Log in to see the delete feature. It also persists to-dos in the browser's local storage for a subsequent visit."
+  },
+  {
+    slug: 'about-dev',
+    title: 'About the dev',
+    description: "I am Madyan, or just call me Ian. I really like building something with coding."
+  },
+]
+
 const SinglePage = () => {
+  const { slug } = useParams()
+  const aboutContent = aboutData.find((item) => item.slug === slug)
+  const { title, description } = aboutContent
+
   return (
     <div className="main_content">
-      single page content
+      <h2>{ title }</h2>
+      <p>{ description }</p>
     </div>
   )
 }
